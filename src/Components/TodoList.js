a0ae4d3684bc0ac9394d8e2c8b603e9358794ddd
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-16 23:14:15 +0300

    added item component to item list

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Item from "./TodoItem"
 
 export default class TodoList extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello from todo list</h1>
-            </div>
+            <section>
+                <h2>todo list</h2>
+                <Item/>
+            </section>
         )
     }
 }
