076eb7baf84af9f9b59c67e12bb6b6ea2d235863
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-16 22:32:24 +0300

    created a structure for the app. TodoList: todolist, todoinput

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from todo Item</h1>
+            </div>
+        )
+    }
+}
