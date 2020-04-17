c3395595e739c790a8940d5893f4208675781199
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-17 14:23:25 +0300

    compleated handleSubmit method

--- src/App.js
+++ src/App.js
@@ -7,30 +7,32 @@ import TodoList from "./Components/TodoList";
 
  class App extends Component {
    state = {
-     items: [
-       {
-         id: 1,
-         title: "wake up",
-       },
-       {
-         id: 2,
-         title: "coding",
-       },
-       {
-         id: 3,
-         title: "read book",
-       },
-     ],
+     items: [ ],
      id: uuidv4(),
      item: "",
      editItem: false,
    };
 
    handleChange = (e) => {
-     console.log("handle change");
+     this.setState({
+       item: e.target.value
+     });
    };
+   
    handleSubmit = (e) => {
-     console.log("handle submit");
+     e.preventDefault();
+      const newItem = {
+        id: this.state.id,
+        title: this.state.item
+      }
+      const updatedItems = [...this.state.items, newItem]
+
+      this.setState({
+        items: updatedItems,
+        item: '',
+        id: uuidv4(),
+        editItem: false
+      })
    };
    clearList = () => {
      console.log("clear list");
